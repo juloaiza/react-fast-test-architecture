6a78739c07924d0dbcc9aef909b202ad9d042095
Julian Loaiza <julian.loaiza@gmail.com>
2022-09-20 15:14:39 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { useEffect, useState } from "react";
-import {BooksComponent} from "./Books/BooksComponent";
+import { BooksComponent } from "./Books/BooksComponent";
 
 export default function App() {
-
   //React View
   return (
     <div>
       <h1>Book List</h1>
-      <BooksComponent/>
+      <BooksComponent />
     </div>
   );
 }
